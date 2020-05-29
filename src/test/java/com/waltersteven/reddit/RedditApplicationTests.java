0b8d860be1e9c50e67e5915e407dc404ea463e16
Walter Pariona <wltrst97@gmail.com>
2020-05-28 23:07:18 -0500

    Added Application properties for DB

---
+++ src/test/java/com/waltersteven/reddit/RedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waltersteven.reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
