cff39be64460fae4864c49deb3c2ff58ed3eda77
Walter Pariona <wltrst97@gmail.com>
2020-05-31 19:38:12 -0500

    Added JWT security services

---
+++ src/main/java/com/waltersteven/reddit/security/JwtProvider.java
@@ -0,0 +1,75 @@
+package com.waltersteven.reddit.security;
+
+import com.waltersteven.reddit.exceptions.SpringRedditException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+import static io.jsonwebtoken.Jwts.parser;
+
+@Service
+public class JwtProvider {
+
+    private KeyStore keyStore;
+
+    @PostConstruct
+    public void init() {
+        try {
+            keyStore = keyStore.getInstance("JKS");
+
+            InputStream resourceAsStream = getClass().getResourceAsStream("/springreddit.jks");
+
+            keyStore.load(resourceAsStream, "secret".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new SpringRedditException("Exception occurred while loading keystore");
+        }
+    }
+
+    public String generateToken(Authentication authentication) {
+        User principal = (User) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .signWith(getPrivateKey())
+                .compact();
+    }
+
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("springreddit", "secret".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            throw new SpringRedditException("Exception occurred while retrieving public key from keystore");
+        }
+    }
+
+    public boolean validateToken(String jwt) {
+        parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
+
+        return true;
+    }
+
+    private PublicKey getPublicKey() {
+        try {
+            return keyStore.getCertificate("springreddit").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new SpringRedditException("Exception occurred while retrieving public key from keystore");
+        }
+    }
+
+    public String getUsername(String jwt) {
+        Claims claims = parser()
+                .setSigningKey(getPublicKey())
+                .parseClaimsJws(jwt)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
