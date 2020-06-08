15835d1431d09fccdf166def09eb5769f220c47f
Walter Pariona <wltrst97@gmail.com>
2020-06-07 22:46:06 -0500

    Added custom find method to voterepository

--- src/main/java/com/waltersteven/reddit/repository/VoteRepository.java
+++ src/main/java/com/waltersteven/reddit/repository/VoteRepository.java
@@ -1,9 +1,14 @@
 package com.waltersteven.reddit.repository;
 
+import com.waltersteven.reddit.model.Post;
+import com.waltersteven.reddit.model.User;
 import com.waltersteven.reddit.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 }
