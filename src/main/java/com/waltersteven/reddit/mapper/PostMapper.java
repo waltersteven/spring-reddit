62326a4ed09ae3e7015c889b7235e4f3827c6948
Walter Pariona <wltrst97@gmail.com>
2020-06-07 20:24:19 -0500

    Added Post mapper class

---
+++ src/main/java/com/waltersteven/reddit/mapper/PostMapper.java
@@ -0,0 +1,47 @@
+package com.waltersteven.reddit.mapper;
+
+import com.github.marlonlom.utilities.timeago.TimeAgo;
+import com.waltersteven.reddit.dto.PostRequest;
+import com.waltersteven.reddit.dto.PostResponse;
+import com.waltersteven.reddit.model.Post;
+import com.waltersteven.reddit.model.Subreddit;
+import com.waltersteven.reddit.model.User;
+import com.waltersteven.reddit.repository.CommentRepository;
+import com.waltersteven.reddit.repository.VoteRepository;
+import com.waltersteven.reddit.service.AuthService;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring")
+public abstract class PostMapper {
+
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private VoteRepository voteRepository;
+    @Autowired
+    private AuthService authService;
+
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "description", source = "postRequest.description")
+    @Mapping(target = "subreddit", source = "subreddit")
+    @Mapping(target = "voteCount", constant = "0")
+    @Mapping(target = "user", source = "user")
+    public abstract Post mapDtoToPost(PostRequest postRequest, Subreddit subreddit, User user);
+
+    @Mapping(target = "id", source = "postId")
+    @Mapping(target = "subredditName", source = "subreddit.name")
+    @Mapping(target = "userName", source = "user.username")
+    @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+    @Mapping(target = "duration", expression = "java(getDuration(post))")
+    public abstract PostResponse mapPostToDto(Post post);
+
+    Integer commentCount(Post post) {
+        return commentRepository.findByPost(post).size();
+    }
+
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
+}
