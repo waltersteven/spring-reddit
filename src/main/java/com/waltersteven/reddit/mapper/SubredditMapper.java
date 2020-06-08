a5ce681b04885dde2c1a77a275be653c2eb7a44a
Walter Pariona <wltrst97@gmail.com>
2020-06-07 20:23:52 -0500

    Added subreddit mapper

--- src/main/java/com/waltersteven/reddit/mapper/SubredditMapper.java
+++ src/main/java/com/waltersteven/reddit/mapper/SubredditMapper.java
@@ -1,4 +1,23 @@
 package com.waltersteven.reddit.mapper;
 
+import com.waltersteven.reddit.dto.SubredditDto;
+import com.waltersteven.reddit.model.Post;
+import com.waltersteven.reddit.model.Subreddit;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> posts) {
+        return posts.size();
+    }
+
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
 }
