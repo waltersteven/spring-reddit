0050145e162916d78e11a0e08994beefcaa51348
Walter Pariona <wltrst97@gmail.com>
2020-06-09 00:46:13 -0500

    Added refresh token service class

---
+++ src/main/java/com/waltersteven/reddit/service/RefreshTokenService.java
@@ -0,0 +1,38 @@
+package com.waltersteven.reddit.service;
+
+import com.waltersteven.reddit.exceptions.SpringRedditException;
+import com.waltersteven.reddit.model.RefreshToken;
+import com.waltersteven.reddit.repository.RefreshTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken generateRefreshToken() {
+        RefreshToken refreshToken = new RefreshToken();
+
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setCreatedDate(Instant.now());
+
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    void validateRefreshToken(String token) {
+        refreshTokenRepository.findByToken(token)
+                .orElseThrow(() -> new SpringRedditException("Invalid refresh token"));
+    }
+
+    public void deleteRefreshToken(String token) {
+        // If no token is found, Spring throws an Illegal Argument Exception
+        refreshTokenRepository.deleteByToken(token);
+    }
+}
