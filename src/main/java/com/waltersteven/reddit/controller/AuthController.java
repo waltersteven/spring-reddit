b0ff5ae4dbf6b7237118ba9e1d4bf0566c77e288
Walter Pariona <wltrst97@gmail.com>
2020-06-09 00:44:33 -0500

    Added methods for refresh token and logout

--- src/main/java/com/waltersteven/reddit/controller/AuthController.java
+++ src/main/java/com/waltersteven/reddit/controller/AuthController.java
@@ -2,12 +2,16 @@ package com.waltersteven.reddit.controller;
 
 import com.waltersteven.reddit.dto.AuthenticationResponse;
 import com.waltersteven.reddit.dto.LoginRequest;
+import com.waltersteven.reddit.dto.RefreshTokenRequest;
 import com.waltersteven.reddit.dto.RegisterRequest;
 import com.waltersteven.reddit.service.AuthService;
+import com.waltersteven.reddit.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
@@ -33,5 +38,17 @@ public class AuthController {
     @PostMapping("/login")
     public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
         return authService.login(loginRequest);
+    }
+
+    @PostMapping("/refresh/token")
+    public AuthenticationResponse refreshToken(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.OK).body("Refresh Token Deleted Successfully!");
     }
 }
